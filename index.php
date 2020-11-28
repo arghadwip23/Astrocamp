ce36a29107823f40d455caba05d1ca0f18369927
Arghadwip Sarkar <68644790+arghadwip23@users.noreply.github.com>
2020-11-28 20:31:54 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php
+$to = "arghadwip23@gmail.com";
+$subject = "HTML email";
+
+$message = "
+<html>
+<head>
+<title>HTML email</title>
+</head>
+<body>
+<p>This email contains HTML Tags!</p>
+<table>
+<tr>
+<th>Firstname</th>
+<th>Lastname</th>
+</tr>
+<tr>
+<td>John</td>
+<td>Doe</td>
+</tr>
+</table>
+</body>
+</html>
+";
+
+// Always set content-type when sending HTML email
+$headers = "MIME-Version: 1.0" . "\r\n";
+$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+
+// More headers
+$headers .= 'From: <arghdwip24@gmail.com>' . "\r\n";
+$headers .= 'Cc: sarkarraja852@gmail.com' . "\r\n";
+
+if(mail($to,$subject,$message,$headers)){
+echo("<h1>email sent you got it</h1>");
+}else{
+echo("<h1>sorry you failed</h1>")
+}
+?>

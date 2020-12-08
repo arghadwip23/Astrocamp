6c9b64874d4066fd28ac6a8668f230189db1d45b
Arghadwip Sarkar <68644790+arghadwip23@users.noreply.github.com>
2020-12-08 15:25:55 +0530

    Update index.php

--- index.php
+++ index.php
@@ -35,7 +35,7 @@
                 <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="neo.html">near earth objects</a>
+                <a class="nav-link" href="neo">near earth objects</a>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="#">Pricing</a>
